491c3e4c5b02774961aa3fce1b1e906a2a8c87f9
AshleyBanton <a.banton@sintlucasedu.nl>
2024-06-12 10:06:57 +0200

    project commit

---
+++ goodfriday/resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<form action="/checkout" method="POST">
+    @csrf
+    <button type="submit">Afrekenen</button>
+</form>
--- goodfriday/routes/web.php
+++ goodfriday/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController; // Ensure you import the controller correctly
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Existing GET route for the index page
+Route::get('/', [PaymentController::class, 'index'])->name('index');
+
+// New POST route for the checkout process
+Route::post('/checkout', [PaymentController::class, 'checkout'])->name('checkout');
+
+// Existing GET route for the success page
+Route::get('/succeed', [PaymentController::class, 'succeed'])->name('succeed');
